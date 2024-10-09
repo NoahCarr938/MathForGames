fed8b082566afa63b66157acf0b487a434295f87
NoahCarr938 <noahcarrbusiness2759@gmail.com>
2024-10-09 16:58:26 -0500

    Add project files.

---
+++ Sandbox/Program.cs
@@ -0,0 +1,83 @@
+﻿using MathLibrary;
+using Raylib_cs;
+using System.Numerics;
+
+
+namespace Sandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 480, "Hello World");
+
+            Vector2 deltaPosition = new Vector2(0, 0);
+            Vector2 position = new Vector2();
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Gray);
+                Raylib.SetTargetFPS(60);
+                Raylib.DrawRectangle((int)position.X, (int)position.Y, 50, 50, Color.Red);
+
+
+                float speed = 100.0f;
+
+                int forward = Raylib.IsKeyDown(KeyboardKey.W);
+                int back = Raylib.IsKeyDown(KeyboardKey.S);
+                int left = Raylib.IsKeyDown(KeyboardKey.A);
+                int right = Raylib.IsKeyDown(KeyboardKey.D);
+
+                deltaPosition.Y -= forward;
+                deltaPosition.Y += back;
+                deltaPosition.X -= left;
+                deltaPosition.X += right;
+
+                position.X = deltaPosition.X;
+                position.Y = deltaPosition.Y;
+
+                
+
+                //Raylib.DrawText("Vector!! That's me. 'Cause I'm committing crimes with both direction and magnitude. OH YEAH!!!!!!!!!!!!", 12, 12, 16, Color.Black);
+                //// Head
+                //Raylib.DrawCircle(375, 150, 55, Color.Beige);
+                //// Glasses
+                //Raylib.DrawRectangle(325, 130, 30, 25, Color.Black);
+                //Raylib.DrawRectangle(395, 130, 30, 25, Color.Black);
+                //Raylib.DrawRectangle(320, 138, 110, 6, Color.Black);
+                //// Eyes
+                //Raylib.DrawRectangle(330, 135, 25, 20, Color.White);
+                //Raylib.DrawRectangle(342, 142, 10, 10, Color.Brown);
+                //Raylib.DrawRectangle(395, 135, 25, 20, Color.White);
+                //Raylib.DrawRectangle(398, 142, 10, 10, Color.Brown);
+                //// Hair
+                //Raylib.DrawRectangle(335, 95, 80, 20, Color.Brown);
+                //Raylib.DrawRectangle(315, 110, 30, 10, Color.Brown);
+                //Raylib.DrawRectangle(395, 110, 30, 10, Color.Brown);
+                //// Body
+                //Raylib.DrawRectangle(325, 200, 100, 200, Color.Orange);
+                //// Arms
+                //Raylib.DrawRectangle(310, 200, 15, 125, Color.Orange);
+                //Raylib.DrawRectangle(425, 200, 15, 125, Color.Orange);
+                //// Hands
+                //Raylib.DrawRectangle(310, 320, 15, 20, Color.Beige);
+                //Raylib.DrawRectangle(425, 320, 15, 20, Color.Beige);
+                //// Zipper
+                //Raylib.DrawRectangle(370, 200, 10, 120, Color.White);
+                //// Legs
+                //Raylib.DrawRectangle(325, 400, 10, 75, Color.Orange);
+                //Raylib.DrawRectangle(415, 400, 10, 75, Color.Orange);
+                //// Shoes
+                //Raylib.DrawRectangle(325, 470, 10, 10, Color.White);
+                //Raylib.DrawRectangle(415, 470, 10, 10, Color.White);
+
+                Raylib.EndDrawing();
+
+
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
