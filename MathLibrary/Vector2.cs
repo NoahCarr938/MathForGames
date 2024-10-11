1ca4be04f435612ac4d40fbc8c7e16e1bc3ecd5a
NoahCarr938 <noahcarrbusiness2759@gmail.com>
2024-10-11 10:26:46 -0500

    Made a Vector 2.

---
+++ MathLibrary/Vector2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace MathLibrary
+{
+    public class Vector2
+    {
+        float x, y;
+
+        public Vector2()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Vector2(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // Operator overload for addition
+        public static Vector2 operator +(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x - right.x, left.y - right.y);
+        }
+
+        //Operator overload for subtraction
+        public static Vector2 operator -(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x - right.x, left.y - right.y);
+        }
+
+        // Operator overload for multiplication by a vector
+        public static Vector2 operator *(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x * right.x, left.y * right.y);
+        }
+
+        // Operator overload for multiplication by a scalar
+        public static Vector2 operator *(Vector2 left, float right)
+        {
+            return new Vector2(left.x * right, left.y * right);
+        }
+
+        // Operator overload for divison
+        public static Vector2 operator /(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x / right.x, left.y / right.y);
+        }
+
+        // implicit conversation from System.Numerics.Vector2 to Vector 2
+        public static implicit operator Vector2(System.Numerics.Vector2 vector)
+        {
+            return new Vector2(vector.X, vector.Y);
+        }
+
+        // implicit conversion from Vector2 to System.Numerics.Vector2
+        public static implicit operator System.Numerics.Vector2(Vector2 vector)
+        {
+            return new System.Numerics.Vector2(vector.x, vector.y);
+        }
+
+        
+    }
+}
