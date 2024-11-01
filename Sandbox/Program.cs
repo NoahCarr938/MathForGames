503d320e8e51a82ba299cd7f7d092808621b907d
NoahCarr938 <noahcarrbusiness2759@gmail.com>
2024-11-01 12:27:31 -0500

    Implemented tests for rectangles, rotations, and parent child relationships.

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -17,11 +17,21 @@ namespace Sandbox
 
             Transform2D t1 = new Transform2D(a);
             t1.LocalScale = new Vector2(100, 100);
-            Vector2 offset = new Vector2(t1.LocalScale.x / 2, t1.LocalScale.y / 2);
+            Vector2 offset = new Vector2(t1.LocalScale.x * SCALE_MULTIPLIER / 2, t1.LocalScale.y * SCALE_MULTIPLIER / 2);
             t1.LocalPosition = new Vector2(
                 (Raylib.GetScreenWidth() * 0.33f) - offset.x,
                 (Raylib.GetScreenHeight() * 0.33f) - offset.y);
-            
+
+
+            Transform2D t2 = new Transform2D(a);
+            t1.AddChild(t2);
+            t2.LocalScale = new Vector2(.5f, .5f);
+            t2.LocalPosition = new Vector2(
+                (100) - offset.x,
+                (50) - offset.y);
+
+            t1.LocalPosition = new Vector2(
+                (Raylib.GetScreenHeight() * 0.33f) - offset.y);
 
             
 
@@ -33,6 +43,7 @@ namespace Sandbox
                 t1.Translate(t1.Forward * 50 * Raylib.GetFrameTime());
                 t1.Rotate(0.5f * Raylib.GetFrameTime());
 
+                t2.Rotate(0.5f * Raylib.GetFrameTime());
 
                 // Draw t1
                 //Raylib.DrawRectanglePro(new Rectangle(t1.GlobalPosition, t1.GlobalScale), t1.GlobalPosition
@@ -42,6 +53,14 @@ namespace Sandbox
                     new Vector2(0, 0) + offset,
                     -t1.GlobalRotationAngle * (180 / (float)Math.PI),
                     Color.Blue);
+
+                // Draw t2
+                rect = new Rectangle(t2.GlobalPosition + offset, t2.GlobalScale * SCALE_MULTIPLIER);
+                Raylib.DrawRectanglePro(
+                    rect,
+                    new Vector2(0, 0) + offset / 2,
+                    -t2.GlobalRotationAngle * (180 / (float)Math.PI),
+                    Color.Green);
 
                 Raylib.DrawLineV(t1.GlobalPosition + offset, t1.GlobalPosition + offset + (t1.Forward * 100), Color.Red);
                 Raylib.EndDrawing();
